542e28aadc3f9fc505cb3218e37cedeee1fca0e4
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2025-04-26 11:39:53 +0100

    put and delete endpoints

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -13,33 +13,33 @@ import static java.util.Calendar.*;
 @Configuration
 public class StudentConfig {
 
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository) {
-        return args -> {
-            Student mariam = new Student(
-                    "Hashim",
-                    "hashim@mail.com",
-                    LocalDate.of(2000, JANUARY + 1, 5),
-                    25
-            );
-
-            Student ayoub = new Student(
-                    "Ayoub",
-                    "ayoub@mail.com",
-                    LocalDate.of(2000, JUNE + 1, 17),
-                    24
-            );
-
-            Student nassim = new Student(
-                    "Nassim",
-                    "nassim@mail.com",
-                    LocalDate.of(1999, MARCH + 1, 22),
-                    26
-            );
-
-            repository.saveAll(
-                    List.of(mariam, ayoub, nassim)
-            );
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository) {
+//        return args -> {
+//            Student mariam = new Student(
+//                    "Hashim",
+//                    "hashim@mail.com",
+//                    LocalDate.of(2000, JANUARY + 1, 5),
+//                    25
+//            );
+//
+//            Student ayoub = new Student(
+//                    "Ayoub",
+//                    "ayoub@mail.com",
+//                    LocalDate.of(2000, JUNE + 1, 17),
+//                    24
+//            );
+//
+//            Student nassim = new Student(
+//                    "Nassim",
+//                    "nassim@mail.com",
+//                    LocalDate.of(1999, MARCH + 1, 22),
+//                    26
+//            );
+//
+//            repository.saveAll(
+//                    List.of(mariam, ayoub, nassim)
+//            );
+//        };
+//    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class StudentService {
@@ -21,6 +22,38 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
-    public void addNewStudent() {
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentsByEmail(student.getEmail());
+        if(studentByEmail.isPresent()) {
+            throw new IllegalStateException("Student with email " + studentByEmail.get().getEmail() + " already exists");
+        }
+        System.out.println(student);
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean found = studentRepository.existsById(studentId);
+        if(!found) {
+            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(String name, String email, Long studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist"));
+
+        if (name != null && !name.isEmpty() && !Objects.equals(student.getName(), name) ) {
+            student.setName(name);
+        }
+        if (email != null && !email.isEmpty() && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentsByEmail(email);
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
